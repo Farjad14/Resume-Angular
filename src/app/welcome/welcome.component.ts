0faa7cc57c46a1a302c6a6c1a0156327175768af
Farjad Abbas <farjad.abbas@cibc.com>
2018-01-08 18:58:04 -0500

    made few changes

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -18,6 +18,7 @@ export class WelcomeComponent implements OnInit {
 
   constructor(private router: Router) {}
   private active: false;
+  private mobile: false;
 
   ngOnInit() {
   }
@@ -25,8 +26,8 @@ export class WelcomeComponent implements OnInit {
     this.active = true;
     $('mat-card-content').css('margin-bottom', '200px');
     setTimeout((router: Router) => {
-      this.router.navigate(['/profile']);
-  }, 3000); 
+      //this.router.navigate(['/profile']);
+  }, 300000); 
   };
 }
 
